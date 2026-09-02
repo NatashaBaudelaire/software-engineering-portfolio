7bccc6b1c3c09a633b4708aa5062128feeeecbea
natashabaudelaire <200302287+NatashaBaudelaire@users.noreply.github.com>
2026-09-02 09:01:12 -0300

    feat: write the Comments class for the vehicle scenarios

---
+++ 07-software-testing/test-case-design/Comments.java
@@ -0,0 +1,14 @@
+package VehicularDevice;
+
+public class Comments {
+/*****************************************************************************************************
+    Vehicle Movement
+    CT035: Put a running, stationary vehicle with the trunk open into motion
+    Door Opening
+    CT051: Open door 2 of a stationary vehicle, with the alarm deactivated and only door 2 closed
+    CT052: Open door 2 of a stationary vehicle, with the alarm deactivated and only door 2 open
+    Door Locking
+    CT073 - Lock door 4 of a vehicle with all doors unlocked
+    CT079 - Lock door 7 of a vehicle with all doors unlocked
+*****************************************************************************************************/
+}
