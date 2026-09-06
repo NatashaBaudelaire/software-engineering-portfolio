954814b9f83498b1eefa8ae007579ef6c2380054
natashabaudelaire <200302287+NatashaBaudelaire@users.noreply.github.com>
2026-09-06 20:06:33 -0300

    refactor: drop the age converter mock

--- 02-python/algorithms-and-logic/variables-and-input/ageconverter.py
+++
@@ -1,7 +0,0 @@
-name = input("Enter your name: ")
-age_years = int(input("Enter your age in years: "))
-
-age_months = age_years * 12
-age_days = age_years * 365
-
-print(f"{name} is {age_years} years old, or {age_months} months, or {age_days} days!")
