43e954ff78df04696698d98207ebcfacfabd3e10
natashabaudelaire <200302287+NatashaBaudelaire@users.noreply.github.com>
2026-09-06 20:06:07 -0300

    feat: check whether a word occurs in the sentence

---
+++ 02-python/algorithms-and-logic/strings/word_presence_checker.py
@@ -0,0 +1,7 @@
+text = input("Enter a text: ")
+word = input("Enter a word: ")
+
+if word in text:
+    print("The word exists in the text")
+else:
+    print("The word does not exist in the text!")
