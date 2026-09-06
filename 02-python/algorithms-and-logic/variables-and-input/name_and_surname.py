277f618a5eec336e7441c3ef46b0aafa6f312b30
natashabaudelaire <200302287+NatashaBaudelaire@users.noreply.github.com>
2026-09-06 20:07:35 -0300

    feat: accept a name and a surname

---
+++ 02-python/algorithms-and-logic/variables-and-input/name_and_surname.py
@@ -0,0 +1,11 @@
+def receive_name(name):
+    print(name)
+
+name = input('Enter your name: ')
+receive_name(name)
+
+def receive_surname(surname):
+    print(surname)
+
+surname = input('Enter your surname: ')
+receive_surname(surname)
