478a58adec14704678b231804e974dc3a4b97033
natashabaudelaire <200302287+NatashaBaudelaire@users.noreply.github.com>
2026-09-06 20:06:11 -0300

    refactor: retire the word presence mock

--- 02-python/algorithms-and-logic/strings/wordpresencechecker.py
+++
@@ -1,7 +0,0 @@
-text = input("Enter a text: ")
-word = input("Enter a word: ")
-
-if word in text:
-    print("The word exists in the text")
-else:
-    print("The word does not exist in the text!")
