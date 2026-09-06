4b6a683d0648eaa31a8cae2f78895f14cdca10e2
natashabaudelaire <200302287+NatashaBaudelaire@users.noreply.github.com>
2026-09-06 20:06:21 -0300

    chore: remove the character-access draft

--- 02-python/algorithms-and-logic/variables-and-input/accessingcharacters.py
+++
@@ -1,8 +0,0 @@
-characters = 'Python'
-indices = [0, 1, 5, 19, 20]
-
-for i in indices:
-    if i < len(characters):
-        print(characters[i])
-    else:
-        print(f"Index {i} is out of range")
