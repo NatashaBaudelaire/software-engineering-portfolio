a45402c2eea881827f897d51037c0db427dab2a5
natashabaudelaire <200302287+NatashaBaudelaire@users.noreply.github.com>
2026-09-06 20:00:51 -0300

    feat: classify characters by operator, number or vowel

---
+++ 02-python/algorithms-and-logic/conditionals/a_e_i_o_u.py
@@ -0,0 +1,10 @@
+character = input().strip()
+
+if character in '+-*/':
+    print("The character is a mathematical operation")
+
+elif character.isnumeric():
+    print("The character is a number")
+
+elif character.lower() in 'aeiou':
+    print("The character is a vowel")
