6b8064cab1665d385bd85540febd7f83ce065a76
natashabaudelaire <200302287+NatashaBaudelaire@users.noreply.github.com>
2026-09-06 20:08:55 -0300

    chore: discard the swap-two-values mock

--- 02-python/algorithms-and-logic/variables-and-input/swaptwovalues.py
+++
@@ -1,9 +0,0 @@
-a = int(input("Enter value for a: "))
-b = int(input("Enter value for b: "))
-
-temp = a
-a = b
-b = temp
-
-print(f"Value of a after swapping: {a}")
-print(f"Value of b after swapping: {b}")
