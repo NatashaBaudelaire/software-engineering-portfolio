c7bb383c1ae1fbcc448515b33472278ef9701157
natashabaudelaire <200302287+NatashaBaudelaire@users.noreply.github.com>
2026-09-08 10:59:20 -0300

    refactor: tidy AddResource and drop dead code

--- 01-java/object-oriented-programming/aggregation-and-compositions/resource/AddResource.java
+++ 01-java/object-oriented-programming/aggregation-and-compositions/resource/AddResource.java
@@ -1,53 +1,7 @@
-package Resource;
+package resource;
 
 import java.util.ArrayList;
 import java.util.List;
-
-
-class Resource {
-    private String name;
-    private String type;
-
-    public Resource(String name, String type) {
-        this.name = name;
-        this.type = type;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getType() {
-        return type;
-    }
-}
-
-
-class Lesson {
-    private String date;
-    private String title;
-    private List<Resource> resources;
-
-    public Lesson(String date, String title) {
-        this.date = date;
-        this.title = title;
-        this.resources = new ArrayList<>();
-    }
-
-    public void addResource(Resource resource) {
-        resources.add(resource);
-    }
-
-    public void showLesson() {
-        System.out.println("Lesson: " + title);
-        System.out.println("Date: " + date);
-        System.out.println("Resources:");
-        for (Resource r : resources) {
-            System.out.println("- " + r.getName() + " (Type: " + r.getType() + ")");
-        }
-    }
-}
-
 
 public class AddResource {
     public static void main(String[] args) {
