ca510bf821d67967f8acfc6ed22f8678537efcdb
natashabaudelaire <200302287+NatashaBaudelaire@users.noreply.github.com>
2026-09-08 10:59:25 -0300

    refactor: refine resource handling in the Lesson class

--- 01-java/object-oriented-programming/aggregation-and-compositions/resource/Lesson.java
+++ 01-java/object-oriented-programming/aggregation-and-compositions/resource/Lesson.java
@@ -1,24 +1,42 @@
-package Lesson;
+package resource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Lesson {
-    private String name;
-    private String type;
+    private String date;
+    private String title;
+    private List<Resource> resources;
 
-    public Lesson(String name, String type) {
-        this.name = name;
-        this.type = type;
+    public Lesson(String date, String title) {
+        this.date = date;
+        this.title = title;
+        this.resources = new ArrayList<>();
+    }
+
+    public void addResource(Resource resource) {
+        resources.add(resource);
+    }
+
+    public void showLesson() {
+        System.out.println("Lesson: " + title);
+        System.out.println("Date: " + date);
+        System.out.println("Resources:");
+        for (Resource r : resources) {
+            System.out.println("- " + r.getName() + " (Type: " + r.getType() + ")");
+        }
     }
 
     public String getName() {
-        return name;
+        return title;
     }
 
     public String getType() {
-        return type;
+        return "Lesson";
     }
 
     @Override
     public String toString() {
-        return name + " (Type: " + type + ")";
+        return title + " (Type: Lesson)";
     }
 }
