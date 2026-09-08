1a6efe7f0e70dbe9763d06e7c16808f1ea31066e
natashabaudelaire <200302287+NatashaBaudelaire@users.noreply.github.com>
2026-09-08 10:59:49 -0300

    refactor: delete the unused message helpers

--- 02-python/algorithms-and-logic/variables-and-input/message_display.py
+++ 02-python/algorithms-and-logic/variables-and-input/message_display.py
@@ -7,13 +7,4 @@ def return_message(msg=None):
 show_message()
 msg = 'test'
 print(return_message(msg))
-def show_message():
-    print('Message shown from the function')
-
-def return_message(msg=None):
-    return 'Returned message'
-
-show_message()
-msg = 'test'
-print(return_message(msg))
     
