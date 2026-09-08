d97d07cb013f03adee4d3784be34bbf74e53dfa3
natashabaudelaire <200302287+NatashaBaudelaire@users.noreply.github.com>
2026-09-08 10:59:44 -0300

    refactor: route VehicleApp through the Vehicle model

--- 01-java/object-oriented-programming/encapsulation/Vehicle/VehicleApp.java
+++ 01-java/object-oriented-programming/encapsulation/Vehicle/VehicleApp.java
@@ -8,36 +8,28 @@ public class VehicleApp {
         manufacturers[0] = new Manufacturer("Volkswagen AG", "Germany");
         manufacturers[1] = new Manufacturer("Toyota Motor Corporation", "Japan");
 
-        VehicleAppList[] vehicles = new VehicleAppList[4];
-        vehicles[0] = new VehicleAppList("P", 50, "Golf", "2022-01-15", manufacturers[0]);
-        vehicles[1] = new VehicleAppList("U", 80, "Transporter", "2021-06-30", manufacturers[0]);
-        vehicles[2] = new VehicleAppList("P", 45, "Corolla", "2023-02-10", manufacturers[1]);
-        vehicles[3] = new VehicleAppList("U", 90, "Hilux", "2022-11-05", manufacturers[1]);
+        Vehicle[] vehicles = new Vehicle[4];
+        vehicles[0] = new Vehicle("P", 50, "Golf", "2022-01-15", manufacturers[0]);
+        vehicles[1] = new Vehicle("U", 80, "Transporter", "2021-06-30", manufacturers[0]);
+        vehicles[2] = new Vehicle("P", 45, "Corolla", "2023-02-10", manufacturers[1]);
+        vehicles[3] = new Vehicle("U", 90, "Hilux", "2022-11-05", manufacturers[1]);
         System.out.println("Registered Vehicles:");
-        for (VehicleAppList v : vehicles) {
-            System.out.printf("ID: %d, Model: %s, Type: %s, Acquisition Date: %s, Manufacturer: %s, Country: %s%n",
-                v.getVehicleId(), v.getModel(), v.getType(), v.getAcquisitionDate(),
-                v.getManufacturer().getCompanyName(), v.getManufacturer().getCountry());
+        for (Vehicle v : vehicles) {
+            System.out.println(v.toString());
         }
 
-        VehicleAppList firstVehicle = vehicles[0];
+        Vehicle firstVehicle = vehicles[0];
         double litersUsed = firstVehicle.fillTank();
-        System.out.printf("%nFilled the first vehicle’s tank with %.2f liters.%n", litersUsed);
-        System.out.printf("ID: %d, Model: %s, Tank Capacity: %d, Liters in Tank: %.2f%n",
-            firstVehicle.getVehicleId(), firstVehicle.getModel(),
-            firstVehicle.getTankCapacity(), firstVehicle.getLitresInTank());
+        System.out.printf("\nFilled the first vehicle's tank with %.2f liters.%n", litersUsed);
+        System.out.println(firstVehicle.toString());
 
         Vehicle thirdVehicle = vehicles[2];
         boolean refuelSuccess = thirdVehicle.refuel(20);
-        System.out.printf("%nAttempt to refuel 20 liters in third vehicle: %s%n", refuelSuccess ? "Success" : "Failed");
-        System.out.printf("ID: %d, Model: %s, Tank Capacity: %d, Liters in Tank: %.2f%n",
-            thirdVehicle.getVehicleId(), thirdVehicle.getModel(),
-            thirdVehicle.getTankCapacity(), thirdVehicle.getLitersInTank());
+        System.out.printf("\nAttempt to refuel 20 liters in third vehicle: %s%n", refuelSuccess ? "Success" : "Failed");
+        System.out.println(thirdVehicle.toString());
 
         boolean consumptionRegistered = thirdVehicle.registerConsumption(10);
-        System.out.printf("%nRegister consumption of 10 liters on third vehicle: %s%n", consumptionRegistered ? "Success" : "Failed");
-        System.out.printf("ID: %d, Model: %s, Tank Capacity: %d, Liters in Tank: %.2f%n",
-            thirdVehicle.getVehicleId(), thirdVehicle.getModel(),
-            thirdVehicle.getTankCapacity(), thirdVehicle.getLitersInTank());
+        System.out.printf("\nRegister consumption of 10 liters on third vehicle: %s%n", consumptionRegistered ? "Success" : "Failed");
+        System.out.println(thirdVehicle.toString());
     }
 }
