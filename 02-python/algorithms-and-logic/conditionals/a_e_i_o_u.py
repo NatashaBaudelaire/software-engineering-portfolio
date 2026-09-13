7f85a2c6488cdd3988cc3aaacd21394be0c70519
natashabaudelaire <200302287+NatashaBaudelaire@users.noreply.github.com>
2026-09-13 12:38:03 -0300

    style: use consistent quotes in a_e_i_o_u.py

--- 02-python/algorithms-and-logic/conditionals/a_e_i_o_u.py
+++ 02-python/algorithms-and-logic/conditionals/a_e_i_o_u.py
@@ -1,10 +1,10 @@
 character = input().strip()
 
-if character in '+-*/':
+if character in "+-*/":
     print("The character is a mathematical operation")
 
 elif character.isnumeric():
     print("The character is a number")
 
-elif character.lower() in 'aeiou':
+elif character.lower() in "aeiou":
     print("The character is a vowel")
